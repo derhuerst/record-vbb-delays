b8f7567124348d3a60d125bdfe76bf316300d1f1
Jannis R <mail@jannisr.de>
2019-02-13 21:26:33 +0100

    use install-unique user agents

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 const createMonitor = require('hafas-monitor-departures')
 const createHafas = require('bvg-hafas')
 const createRecorder = require('hafas-record-delays')
+const id = require('./id.json')
 
-const hafas = createHafas('record-vbb-delays')
+const hafas = createHafas('record-vbb-delays ' + id)
 
 const record = (stations, interval, dbPath) => {
 	const monitor = createMonitor(hafas, stations, interval)
